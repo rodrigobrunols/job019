09f1bfd5e279f2dbd7ef8fe5a1d357b3a010f8c8
rodrigo.leoncio <rodrigo.leoncio@sicoob.com.br>
2026-02-21 09:49:50 -0300

    first commit

---
+++ src/main/java/com/thaloz/job019/Job019Application.java
@@ -0,0 +1,13 @@
+package com.thaloz.job019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job019Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/thaloz/job019/Job019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaloz.job019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job019ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
